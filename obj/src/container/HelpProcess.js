d1a3e392a1f5b8ccb82747bfa6a715e8df01e946
Dmitriy Krayniy <krdima92@gmail.com>
2018-08-20 14:05:28 +0300

    added DefaultRpcFactory

--- obj/src/container/HelpProcess.js
+++ obj/src/container/HelpProcess.js
@@ -2,10 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const pip_services_container_node_1 = require("pip-services-container-node");
 const HelpServiceFactory_1 = require("../build/HelpServiceFactory");
+const pip_services_rpc_node_1 = require("pip-services-rpc-node");
 class HelpProcess extends pip_services_container_node_1.ProcessContainer {
     constructor() {
         super("help", "Context help microservice");
         this._factories.add(new HelpServiceFactory_1.HelpServiceFactory);
+        this._factories.add(new pip_services_rpc_node_1.DefaultRpcFactory);
     }
 }
 exports.HelpProcess = HelpProcess;
--- src/container/HelpProcess.ts
+++ src/container/HelpProcess.ts
@@ -2,12 +2,14 @@ import { IReferences } from 'pip-services-commons-node';
 import { ProcessContainer } from 'pip-services-container-node';
 
 import { HelpServiceFactory } from '../build/HelpServiceFactory';
+import { DefaultRpcFactory } from 'pip-services-rpc-node';
 
 export class HelpProcess extends ProcessContainer {
 
     public constructor() {
         super("help", "Context help microservice");
         this._factories.add(new HelpServiceFactory);
+        this._factories.add(new DefaultRpcFactory);
     }
 
 }
