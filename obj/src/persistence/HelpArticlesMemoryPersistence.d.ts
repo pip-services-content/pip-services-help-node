bea7b8c5adea285834697571eb167cf126280f8c
pipdeveloper <pipdevs@gmail.com>
2019-04-23 12:55:24 +0000

    project build by Travis CI [skip ci]

--- obj/src/persistence/HelpArticlesMemoryPersistence.d.ts
+++ obj/src/persistence/HelpArticlesMemoryPersistence.d.ts
@@ -6,8 +6,8 @@ import { HelpArticleV1 } from '../data/version1/HelpArticleV1';
 import { IHelpArticlesPersistence } from './IHelpArticlesPersistence';
 export declare class HelpArticlesMemoryPersistence extends IdentifiableMemoryPersistence<HelpArticleV1, string> implements IHelpArticlesPersistence {
     constructor();
-    private contains;
-    private composeFilter;
+    private contains(array1, array2);
+    private composeFilter(filter);
     getPageByFilter(correlationId: string, filter: FilterParams, paging: PagingParams, callback: (err: any, page: DataPage<HelpArticleV1>) => void): void;
     getOneRandom(correlationId: string, filter: FilterParams, callback: (err: any, item: HelpArticleV1) => void): void;
 }
